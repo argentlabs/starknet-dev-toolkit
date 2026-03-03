e05621843cd06a2b539e8c0eacdf2ed138b68525
gaetbout <gaetan.ansotte@hotmail.com>
2026-03-03 09:30:00 +0100

    shims: adding join in path

--- browser-shims/path.js
+++ browser-shims/path.js
@@ -3,4 +3,4 @@ import { createShim } from "./shim.js";
 export const sep = "/";
 const path = Object.assign(createShim("path"), { sep });
 export default path;
-export const { resolve, dirname } = path;
+export const { resolve, dirname, join } = path;
